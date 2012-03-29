3f44f6f657d580d4988f99078cc456e8c3d0641a
AtsushiM <at.mizoue@gmail.com>
2012-03-29 09:54:21 +0900

    update

--- js/main.js
+++ js/main.js
@@ -1,16 +1,41 @@
-var XXX = (function() {
+var XXX = (function(win, doc, $) {
+    //mobile check
+    var isMobile = (function() {
+        var m = navigator.userAgent.match;
+
+        return {
+            Android: function() {
+                return m(/Android/i) ? true : false;
+            },
+            iOS: function() {
+                return m(/iPhone|iPad|iPod/i) ? true : false;
+            },
+            Windows: function() {
+                return m(/IEMobile/i) ? true : false;
+            },
+            any: function() {
+                return (
+                    isMobile.Android() ||
+                    isMobile.BlackBerry() ||
+                    isMobile.iOS() ||
+                    isMobile.Windows()
+                );
+            }
+        };
+    }());
+
     //scrollbar
     (function() {
-        window.addEventListener('load', function() {
+        win.addEventListener('load', function() {
             setTimeout(doScroll, 100);
         }, false);
-        window.onorientationchange = function() {
+        win.onorientationchange = function() {
             setTimeout(doScroll, 100);
         };
         function doScroll() {
-            if (window.pageYOffset === 0) {
-                window.scrollTo(0, 1);
+            if (win.pageYOffset === 0) {
+                win.scrollTo(0, 1);
             }
         }
-    });
-}());
+    }());
+}(window, document, jQuery));
